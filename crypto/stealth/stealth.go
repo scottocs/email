3d88d3849e6a03524bb6a9317098b8843ade1795
zhangliang <briliasm@qq.com>
2024-06-17 08:36:07 +0800

    proposal

--- crypto/stealth/stealth.go
+++ crypto/stealth/stealth.go
@@ -1,4 +1,4 @@
 package stealth
 
-//TODO refer to https://github.com/liyue201/stealth-address-demo/blob/main/main.go
-// or refer to teststealth.go
+//TODO refer to https://github.com/liyue201/stealth-address-demo/blob/main/main.go i.e., teststealth.go
+// or refer to https://github.com/hacheigriega/go-stealth/blob/main/stealth.go
